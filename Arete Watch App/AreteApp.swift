6a0cd9d9e2e9024a1f845ed18438ab873d2e00fa
Peyton Sherwood <peytons@gmail.com>
2025-06-19 14:56:09 -0500

    Initial Commit

---
+++ Arete Watch App/AreteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreteApp.swift
+//  Arete Watch App
+//
+//  Created by Peyton Sherwood on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Arete_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Arete Watch AppTests/Arete_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Arete_Watch_AppTests.swift
+//  Arete Watch AppTests
+//
+//  Created by Peyton Sherwood on 6/19/25.
+//
+
+import Testing
+@testable import Arete_Watch_App
+
+struct Arete_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
